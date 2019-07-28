2ba758518e9b5212a1c1da680af8d75fc134c04f
snookerhd <47082312+snookerhd@users.noreply.github.com>
2019-07-28 10:47:16 +0200

    Add files via upload

--- main/java/com/izako/HunterX/init/ModItems.java
+++ main/java/com/izako/HunterX/init/ModItems.java
@@ -11,6 +11,7 @@ import com.izako.HunterX.items.tools.GonsFishingRod;
 import com.izako.HunterX.items.tools.ItemCard;
 import com.izako.HunterX.items.tools.ItemNeedle;
 import com.izako.HunterX.items.tools.KurapikasSword;
+import com.izako.HunterX.items.tools.Pistol;
 import com.izako.HunterX.items.tools.ToolSword;
 import com.izako.HunterX.items.tools.Yoyo;
 import com.izako.HunterX.util.Reference;
@@ -88,6 +89,7 @@ public class ModItems {
 	public static final ItemCard HISOKAS_CARD = new ItemCard("hisokas_card");
 	public static final ItemNeedle ILLUMIS_NEEDLE = new ItemNeedle("illumis_needle");
 	public static final GonsFishingRod GONS_FISHING_ROD = new GonsFishingRod("gons_fishing_rod");
+	public static final Pistol PISTOL = new Pistol ("pistol");
 
 //Armor
 
