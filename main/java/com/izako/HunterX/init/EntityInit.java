0e004a39f07b9186335eeeff2a455c063278db85
snookerhd <47082312+snookerhd@users.noreply.github.com>
2019-08-18 17:02:20 +0200

    Add files via upload

--- main/java/com/izako/HunterX/init/EntityInit.java
+++ main/java/com/izako/HunterX/init/EntityInit.java
@@ -3,6 +3,7 @@ package com.izako.HunterX.init;
 import com.izako.HunterX.Main;
 import com.izako.HunterX.entity.BossExam;
 import com.izako.HunterX.entity.Empty;
+import com.izako.HunterX.entity.EntityKiriko;
 import com.izako.HunterX.entity.EntityStats;
 import com.izako.HunterX.entity.Examiner;
 import com.izako.HunterX.entity.Thug;
@@ -24,50 +25,52 @@ import net.minecraftforge.fml.common.registry.EntityRegistry;
 import net.minecraftforge.fml.common.registry.ForgeRegistries;
 
 public class EntityInit {
-	
+
 	public static void registerEntities() {
 		registerEntity("Thug", Thug.class, Reference.THUG, 50, 65280, 17664);
 		registerEntity("Examiner", Examiner.class, Reference.EXAMINER, 50, 0, 999999);
 		registerEntity("BossExam", BossExam.class, Reference.BOSSEXAM, 50, 001011, 101101);
 		registerEntity("EntityStats", EntityStats.class, Reference.ENTITYSTATS, 50, 110100, 010010);
-		EntityRegistry.registerModEntity(new ResourceLocation(Reference.MOD_ID + ":" + "EntityCard"), EntityCard.class, "EntityCard", 124, Main.instance, 64, 10, true);
-		EntityRegistry.registerModEntity(new ResourceLocation(Reference.MOD_ID + ":" + "EntityNeedle"), EntityNeedle.class, "EntityCard", 125, Main.instance, 64, 10, true);
-	    EntityRegistry.registerModEntity(new ResourceLocation(Reference.MOD_ID + ":" + "EntityCard"), YoyoProjectile.class, "YoyoProjectile", 126, Main.instance, 64, 10, true);
+		registerEntity("EntityKiriko", EntityKiriko.class, Reference.ENTITYKIRIKO, 50, 641641, 101101);
+		EntityRegistry.registerModEntity(new ResourceLocation(Reference.MOD_ID + ":" + "EntityCard"), EntityCard.class,
+				"EntityCard", 124, Main.instance, 64, 10, true);
+		EntityRegistry.registerModEntity(new ResourceLocation(Reference.MOD_ID + ":" + "EntityNeedle"),
+				EntityNeedle.class, "EntityCard", 125, Main.instance, 64, 10, true);
+		EntityRegistry.registerModEntity(new ResourceLocation(Reference.MOD_ID + ":" + "EntityCard"),
+				YoyoProjectile.class, "YoyoProjectile", 126, Main.instance, 64, 10, true);
 
 		addSpawns();
 	}
-	
-	private static void registerEntity(String name, Class<? extends Entity> entity, int id, int range, int color1, int color2) {
-		EntityRegistry.registerModEntity(new ResourceLocation(Reference.MOD_ID + ":" + name), entity, name, id, Main.instance, range, 1, true, color1, color2);
-		
-	}
-	
-	
-	 private static void addSpawns() {
-		 copySpawns(EntityStats.class,EnumCreatureType.MONSTER, EntityCreeper.class, EnumCreatureType.MONSTER, 100, 1);
-		 
-		 copySpawns(Empty.class,EnumCreatureType.MONSTER, EntityCreeper.class, EnumCreatureType.MONSTER, 100, 1);
-		 
-		 copySpawns(Thug.class, EnumCreatureType.MONSTER, EntityCreeper.class, EnumCreatureType.MONSTER, 10, 1);
-		 
-		 copySpawns(Examiner.class, EnumCreatureType.MONSTER, EntityCreeper.class, EnumCreatureType.MONSTER, 1, 1);
-		  
-		  
-		 }
-		 
-		 private static Biome[] getBiomes(final BiomeDictionary.Type type) {
-		  return BiomeDictionary.getBiomes(type).toArray(new Biome[0]);
-		 }
 
-	 private static void copySpawns(final Class<? extends EntityLiving> classToAdd, final EnumCreatureType creatureTypeToAdd, final Class<? extends EntityLiving> classToCopy, final EnumCreatureType creatureTypeToCopy, int spawnrate, int MaxInGroup) {
-		  for (final Biome biome : ForgeRegistries.BIOMES) {
-		   biome.getSpawnableList(creatureTypeToCopy).stream()
-		     .filter(entry -> entry.entityClass == classToCopy)
-		     .findFirst()
-		     .ifPresent(spawnListEntry ->biome
-		     .getSpawnableList(creatureTypeToAdd)
-		     .add(new Biome.SpawnListEntry(classToAdd, spawnrate, 1, 1)));
-		  }
+	private static void registerEntity(String name, Class<? extends Entity> entity, int id, int range, int color1,
+			int color2) {
+		EntityRegistry.registerModEntity(new ResourceLocation(Reference.MOD_ID + ":" + name), entity, name, id,
+				Main.instance, range, 1, true, color1, color2);
+
+	}
+
+	private static void addSpawns() {
+		copySpawns(EntityStats.class, EnumCreatureType.MONSTER, EntityCreeper.class, EnumCreatureType.MONSTER, 100, 1);
+
+		copySpawns(Empty.class, EnumCreatureType.MONSTER, EntityCreeper.class, EnumCreatureType.MONSTER, 100, 1);
+
+		copySpawns(Thug.class, EnumCreatureType.MONSTER, EntityCreeper.class, EnumCreatureType.MONSTER, 10, 1);
+
+		copySpawns(Examiner.class, EnumCreatureType.MONSTER, EntityCreeper.class, EnumCreatureType.MONSTER, 1, 1);
+
+	}
+
+	private static Biome[] getBiomes(final BiomeDictionary.Type type) {
+		return BiomeDictionary.getBiomes(type).toArray(new Biome[0]);
+	}
+
+	private static void copySpawns(final Class<? extends EntityLiving> classToAdd,
+			final EnumCreatureType creatureTypeToAdd, final Class<? extends EntityLiving> classToCopy,
+			final EnumCreatureType creatureTypeToCopy, int spawnrate, int MaxInGroup) {
+		for (final Biome biome : ForgeRegistries.BIOMES) {
+			biome.getSpawnableList(creatureTypeToCopy).stream().filter(entry -> entry.entityClass == classToCopy)
+					.findFirst().ifPresent(spawnListEntry -> biome.getSpawnableList(creatureTypeToAdd)
+							.add(new Biome.SpawnListEntry(classToAdd, spawnrate, 1, 1)));
 		}
-		} 
-		
+	}
+}
